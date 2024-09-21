87be96c47fd3f5bf96ddb1ea9a5c0ab6710629b0
akshi-05-ai <akshitachauhan7500@gmail.com>
2024-09-21 19:48:48 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountID = 14432
+let accountEmail = "akshita@gmail.com"
+var accountPassword = "123467"
+accountCity = "Delhi"
+
+//accountID = 2//not allowed
+
+accountEmail = "ac@g.com"
+accountPassword = "212117236"
+accountCity = "america"
+let accountState;
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
+
