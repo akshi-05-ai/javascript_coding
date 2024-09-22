f360b531738ad92b5f8287e26b409f4c0fd83132
akshi-05-ai <akshitachauhan7500@gmail.com>
2024-09-22 10:22:05 +0000

    conversions

---
+++ 01_basics/conversion.js
@@ -0,0 +1,17 @@
+let score = "32"
+
+console.log(typeof score);
+
+let valueIntNumber = Number(score)
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+//"" => false
+//"akshi" => true
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
