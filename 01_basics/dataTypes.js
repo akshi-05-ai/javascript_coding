80cc667b4ea1a791788297e7cf41161c30caa6f7
akshi-05-ai <akshitachauhan7500@gmail.com>
2024-09-22 09:22:35 +0000

    data types

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,12 @@
+"user strict";
+let age = 18
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+//object
+
+console.log(typeof age);
