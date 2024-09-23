b3ce3699413cc91fd6b20ad98ac8b64d6a0d3771
akshi-05-ai <akshitachauhan7500@gmail.com>
2024-09-23 18:04:43 +0000

    strings

---
+++ 01_basics/string.js
@@ -0,0 +1,11 @@
+let str1 = "hello"
+let str2 = " coders"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+
+console.log(3 + 2 * 5 % 3);
