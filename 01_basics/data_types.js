8508e73e46d6ee9fe2196852c87747b57041c0d4
akshi-05-ai <akshitachauhan7500@gmail.com>
2024-09-24 15:41:30 +0000

    operators

---
+++ 01_basics/data_types.js
@@ -0,0 +1,30 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, symbol
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+//Non Primitive 
+// array, objects, functions 
+
+const heros = ["shaktiman", "hulk"];
+let myObj = {
+    name: "akshu"
+    age: 18,
+}
+
+const myFunction = function(){
+    console.log("hello");
+    
+}
